26f5411476dfd2a262aff6dbbcef311c5beeaa36
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-05-18 21:06:25 +0530

    18-05-2024

---
+++ May-2024/18-05-2024.cpp
@@ -0,0 +1,43 @@
+/*
+Author : Vandana Kumari 
+Date : 18/05/2024
+Problem : Find the Highest number
+Problem Link : https://www.geeksforgeeks.org/problems/find-the-highest-number2259/1
+*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        // Code here.
+        return *max_element(a.begin(), a.end());
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
