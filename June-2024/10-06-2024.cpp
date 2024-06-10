0e49acf7bf1438e3f9acff5b999fad1c5b6f41cc
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-06-10 11:18:44 +0530

    10-06-2024

---
+++ June-2024/10-06-2024.cpp
@@ -0,0 +1,56 @@
+/*
+Author : Vandana Kumari 
+Date : 10/06/2024
+Problem : Nuts and Bolts Problem
+Problem Link : https://www.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+        // Sort the nuts and bolts arrays
+        sort(nuts, nuts + n);
+        sort(bolts, bolts + n);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        char nuts[n], bolts[n];
+        for (int i = 0; i < n; i++) {
+            cin >> nuts[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> bolts[i];
+        }
+        Solution ob;
+        ob.matchPairs(n, nuts, bolts);
+        for (int i = 0; i < n; i++) {
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
