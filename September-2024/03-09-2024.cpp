bbbd92dcc0a847d880b14266a72cdf5f879b68d3
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-09-03 21:08:41 +0530

    03-09-2024

---
+++ September-2024/03-09-2024.cpp
@@ -0,0 +1,61 @@
+/*
+Author : Vandana Kumari 
+Date : 03/09/2024
+Problem : Minimum number of deletions and insertions
+Problem Link : https://www.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    // Your code goes here
+	    int m = str1.size();
+        int n = str2.size();
+
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    curr[j] = prev[j - 1] + 1;
+                } else {
+                    curr[j] = max(prev[j], curr[j - 1]);
+                }
+            }
+            prev = curr;
+        }
+
+        return m + n - 2 * curr[n];
+	    
+	} 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
