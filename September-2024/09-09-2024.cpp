2adfdcb552a5629c385944084b960d9fd0155480
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-09-09 22:55:07 +0530

    09-09-2024

---
+++ September-2024/09-09-2024.cpp
@@ -0,0 +1,67 @@
+/*
+Author : Vandana Kumari 
+Date : 09/09/2024
+Problem : Sort 0s, 1s and 2s
+Problem Link : https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int low = 0, mid = 0, high = arr.size() - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            } 
+            else if (arr[mid] == 1) {
+                mid++;
+            } 
+            else {  
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
